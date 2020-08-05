8043babf510eb6ba311d58e158b292bd9b16ffc5
lahidu lakshan <lahidulakshan16@gmail.com>
2020-08-05 22:53:48 +0530

    final

---
+++ console.R
@@ -0,0 +1,26 @@
+source("E:\\applide science degree\\coursera\\r\\swirl\\Peer-graded-Assignment-Programming-Assignment-2-Lexical-Scoping\\makeCacheMatrix.R")
+
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+
+
+my_matrix$get()
+
+my_matrix$getInverse()
+
+cacheSolve(my_matrix)
+
+cacheSolve(my_matrix)
+
+my_matrix$getInverse()
+
+my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+
+my_matrix$get()
+
+my_matrix$getInverse()
+
+cacheSolve(my_matrix)
+
+cacheSolve(my_matrix)
+
+my_matrix$getInverse()
